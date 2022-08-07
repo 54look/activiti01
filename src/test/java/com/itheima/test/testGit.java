6ba43b940a337b669d10bd144c98cbc2e17a1bc0
look_blog <251962672@qq.com>
2022-08-07 21:22:06 +0800

    air create file

---
+++ src/test/java/com/itheima/test/testGit.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+/**
+ * @Package: com.itheima.test
+ * @ClassName: testGit
+ * @Author: h2cloud
+ * @CreateTime: 2022/8/7 9:18 下午
+ * @Description:
+ */
+public class testGit {
+	public static void main(String[] args) {
+		System.out.println("this is my first file");
+	}
+}
