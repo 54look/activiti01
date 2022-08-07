b9244aff36bd6bcbea2795fa0d76c91fc2928f6c
look_blog <251962672@qq.com>
2022-08-07 21:46:13 +0800

    air create testGit2

---
+++ src/test/java/com/itheima/test/testGit2.java
@@ -0,0 +1,14 @@
+package com.itheima.test;
+
+/**
+ * @Package: com.itheima.test
+ * @ClassName: testGit2
+ * @Author: h2cloud
+ * @CreateTime: 2022/8/7 9:45 下午
+ * @Description:
+ */
+public class testGit2 {
+	public static void main(String[] args) {
+
+	}
+}
