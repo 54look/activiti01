cbedaddae073483df43042b48404e6746680f4d0
wulongkai <wulongkai@58.com>
2022-08-15 10:33:22 +0800

    update testGit2.java

--- src/test/java/com/itheima/test/testGit2.java
+++ src/test/java/com/itheima/test/testGit2.java
@@ -10,6 +10,9 @@ package com.itheima.test;
 public class testGit2 {
 	public static void main(String[] args) {
 //......
-		System.out.println("Mac 1");
+
+		System.out.println(Runtime.getRuntime().availableProcessors()); //输出本机CPU的数量,是一个数字
+
+
 	}
 }
